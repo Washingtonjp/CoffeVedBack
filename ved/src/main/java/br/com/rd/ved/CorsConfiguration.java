c17d6d1dc74c87d873f21806d8afb4e8f2b6babd
Washingtonjp <washingtondejesuspereira@hotmail.com>
2024-11-25 22:46:55 -0300

    atualizacao hospedagem

--- ved/src/main/java/br/com/rd/ved/CorsConfiguration.java
+++ ved/src/main/java/br/com/rd/ved/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration  implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000","https://viacep.com.br/","https://cofffeved.netlify.app/")
+            .allowedOrigins("http://localhost:3000","https://viacep.com.br/","https://cofffeved.netlify.app/","https://coffevedback-production.up.railway.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
